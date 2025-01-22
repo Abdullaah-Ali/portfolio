c39383eb27baf16be5c9d28bf3eac611feb5f656
Syed Abdullah Ali Quadri <134156961+Abdullaah-Ali@users.noreply.github.com>
2025-01-22 14:48:21 +0500

    fix the issue of from react to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Remove './public/index.html'
   theme: {
     extend: {
       fontFamily: {
-        'poppins': ['Poppins', 'sans-serif'],
-        'lato': ['Lato', 'sans-serif'],
-        'sans': ['Inter', 'Arial', 'sans-serif'],  // Ensure `sans` is defined
-        'mono': ['Menlo', 'Monaco', 'Courier New', 'monospace'],  // Ensure `mono` is defined
-        'quicksand': ['Quicksand', 'sans-serif'],  // Add Quick Sand here
-        'inter': ['Inter', 'sans-serif'],  // Add Open Sans here
-        'roboto': ['Roboto', 'sans-serif'],  // Add Open Sans here
+        poppins: ['Poppins', 'sans-serif'],
+        lato: ['Lato', 'sans-serif'],
+        sans: ['Inter', 'Arial', 'sans-serif'], // Ensure `sans` is defined
+        mono: ['Menlo', 'Monaco', 'Courier New', 'monospace'], // Ensure `mono` is defined
+        quicksand: ['Quicksand', 'sans-serif'], // Add Quick Sand here
+        inter: ['Inter', 'sans-serif'], // Add Open Sans here
+        roboto: ['Roboto', 'sans-serif'], // Add Open Sans here
       },
     },
   },
